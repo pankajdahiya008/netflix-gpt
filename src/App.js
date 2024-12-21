eec3ddd108a53428bb93c5ab06c312a479b2f367
Pankaj Dahiya <pankaj.id.008@gmail.com>
2024-12-21 12:55:34 +0530

    added vercel speed insights

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Provider } from "react-redux";
 import Body from "./components/Body";
 import appStore from "./utils/appStore";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
     <Provider store={appStore}>
+      <SpeedInsights />
       <Body />
     </Provider>
   );
