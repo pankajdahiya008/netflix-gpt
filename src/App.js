49a9815e798e49eeeb65b819ded49bae1d08c9d4
Pankaj Dahiya <pankaj.id.008@gmail.com>
2024-12-21 13:03:24 +0530

    added traffic analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Provider } from "react-redux";
 import Body from "./components/Body";
 import appStore from "./utils/appStore";
+import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
     <Provider store={appStore}>
+      <Analytics/>
       <SpeedInsights />
       <Body />
     </Provider>
